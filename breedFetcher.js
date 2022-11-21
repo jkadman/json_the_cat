dcdf72f89d861919d8d61e2ab6866d0ec3d81fa4
Jason Kadman <jkadman231@gmail.com>
2022-11-21 06:36:16 +0000

    added edge case and finish project

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,12 @@
 const request = require('request');
+
+const breed = process.argv[2];
+
+request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
+  if (!error) {
+    const data = JSON.parse(body);
+    console.log(data[0]);
+  } else {
+    console.log(error);
+  }
+});
