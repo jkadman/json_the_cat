a2e3341961be93e4683f33fcd502d9db9f5a4c13
Jason Kadman <jkadman231@gmail.com>
2022-11-21 17:16:00 +0000

    backend for the cat fetch api

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,12 +1,22 @@
 const request = require('request');
-
 const breed = process.argv[2];
 
-request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
-  if (!error) {
-    const data = JSON.parse(body);
-    console.log(data[0]);
-  } else {
-    console.log(error);
-  }
-});
+const fetchBreedDescription = function(breed, callback) {
+  request('https://api.thecatapi.com/v1/breeds/search?q=' + breed, (error, response, body) => {
+  // convert body from string to object  
+  const data = JSON.parse(body);
+  if (error) {
+      callback(error, null);
+      return;
+    }
+    if (!data.length) {
+      callback('no such breed')
+      return
+    }
+    
+    callback(null, data[0].description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
